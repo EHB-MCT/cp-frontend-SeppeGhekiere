e09dcc05616bf2e6a3c77171b9a970b680db2de7
SeppeGhekiere <seppe.ghekiere@student.ehb.be>
2025-05-24 14:39:00 +0200

    changed nav, so correct styling for current page

--- src/modules/app/components/Navigation.tsx
+++ src/modules/app/components/Navigation.tsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import "../../../styles/header.scss";
 
 export const Navigation = () => {
+	const location = useLocation();
+
 	return (
 		<div className="nav">
-			<Link to="/" className="clicked">
+			<Link to="/" className={location.pathname === "/" ? "clicked" : ""}>
 				Home
 			</Link>
-			<Link to="/all-projects">All Projects</Link>
-			<Link to="/making-of">Making Of</Link>
+			<Link to="/all-projects" className={location.pathname === "/all-projects" ? "clicked" : ""}>
+				All Projects
+			</Link>
+			<Link to="/making-of" className={location.pathname === "/making-of" ? "clicked" : ""}>
+				Making Of
+			</Link>
 
-			<Link to="/fairy-tale">Sprookje</Link>
+			<Link to="/fairy-tale" className={location.pathname === "/fairy-tale" ? "clicked" : ""}>
+				Sprookje
+			</Link>
 		</div>
 	);
 };
